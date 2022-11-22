9279309e5c1c8e006567ace9a4c7f4220485f65a
tincan-mvk <81586601+tincan-mvk@users.noreply.github.com>
2022-11-22 20:22:13 +0100

    Update my_rgb_colors.py

--- my_rgb_colors.py
+++ my_rgb_colors.py
@@ -406,4 +406,59 @@ class my_rgb_colors(object):
         return red, green, blue
     
     def hex_to_rgb(hex_val): #'#4285f4'
-        return tuple(int(hex_val.lstrip('#')[ii:ii+2],16) for ii in (0,2,4))
+        return tuple(int(hex_val.lstrip('#')[ii:ii+2],16) for ii in (0,2,4)
+                     
+    def colorHSV(self, hue, sat, val):
+            """
+            seen at https://github.com/blaz-r/pi_pico_neopixel/neopixel.py
+            Converts HSV color to rgb tuple and returns it.
+            The logic is almost the same as in Adafruit NeoPixel library:
+            https://github.com/adafruit/Adafruit_NeoPixel so all the credits for that
+            go directly to them (license: https://github.com/adafruit/Adafruit_NeoPixel/blob/master/COPYING)
+            :param hue: Hue component. Should be on interval 0..65535
+            :param sat: Saturation component. Should be on interval 0..255
+            :param val: Value component. Should be on interval 0..255
+            :return: (r, g, b) tuple
+            """
+            if hue >= 65536:
+                hue %= 65536
+
+            hue = (hue * 1530 + 32768) // 65536
+            if hue < 510:
+                b = 0
+                if hue < 255:
+                    r = 255
+                    g = hue
+                else:
+                    r = 510 - hue
+                    g = 255
+            elif hue < 1020:
+                r = 0
+                if hue < 765:
+                    g = 255
+                    b = hue - 510
+                else:
+                    g = 1020 - hue
+                    b = 255
+            elif hue < 1530:
+                g = 0
+                if hue < 1275:
+                    r = hue - 1020
+                    b = 255
+                else:
+                    r = 255
+                    b = 1530 - hue
+            else:
+                r = 255
+                g = 0
+                b = 0
+
+            v1 = 1 + val
+            s1 = 1 + sat
+            s2 = 255 - sat
+
+            r = ((((r * s1) >> 8) + s2) * v1) >> 8
+            g = ((((g * s1) >> 8) + s2) * v1) >> 8
+            b = ((((b * s1) >> 8) + s2) * v1) >> 8
+
+            return r, g, b
